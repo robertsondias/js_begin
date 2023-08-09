6b09fc4d23a2d1009a22ac872c691d9f332c42ff
robertsondias <robertsondias@gmail.com>
2023-08-09 11:36:38 -0300

    "Add"

---
+++ funcoes.js
@@ -0,0 +1,35 @@
+// function exibirMsg(mensagem, autor){
+//     console.log('Mensagem:' + mensagem + 'Autor: ' + autor);
+// }
+// // exibirMsg('Aula de Javascript');
+// // exibirMsg('Robertson');
+// // exibirMsg(1000);
+// // exibirMsg({nome: 'Maria', idade: 30});
+
+// exibirMsg('Encontre me. ', 'Deus');
+
+// function somar(x,y) {
+// return(x+y)
+// }
+
+// var resultado = somar(7,7);
+// console.log(resultado);
+//_____________________________________________
+
+// function pesquisar(){
+//     var pesquisa = document.getElementById('pesquisa');
+//     console.log(pesquisa.value);
+// }
+// //____________________________________________________________
+
+function multiplicar (x,y){
+    return x*y;
+}
+console.log(multiplicar(10,8));
+
+//    OU
+
+var mult = (x,y) => {
+    return x*y;
+}
+console.log(mult(2,10));
